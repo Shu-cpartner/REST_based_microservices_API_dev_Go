bc289f781b41ad7cdd331ade86e9efc394e2ce17
Shuji <zaha@cprtnr.com>
2022-11-06 11:06:38 +0900

    Refactor error handling

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -2,19 +2,12 @@ package app
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"microservicesAPIDevInGolang/service"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
-}
 
 // Privary portのServiceとの依存性を定義
 type CustomerHandlers struct {
@@ -35,11 +28,16 @@ func (ch *CustomerHandlers) getCustomer(w http.ResponseWriter, r *http.Request) 
 	id := vars["customer_id"]
 	customer, err := ch.service.GetCustomer(id)
 	if err != nil {
-		w.WriteHeader(err.Code)
-		fmt.Fprintf(w, err.Message)
+		writeResponse(w, err.Code, err.AsMessage())
 	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customer)
+		writeResponse(w, http.StatusOK, customer)
 	}
+}
 
+func writeResponse(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
+	}
 }
--- errs/errors.go
+++ errs/errors.go
@@ -5,8 +5,14 @@ import (
 )
 
 type AppError struct {
-	Message string
-	Code    int
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func (e AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
 }
 
 func NewNotFoundError(messega string) *AppError {
