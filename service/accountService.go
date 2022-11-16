0c3210940afee2d26807015e12e3723398bae9b6
Shuji <zaha@cprtnr.com>
2022-11-17 05:35:06 +0900

    New Bank Account Part1

---
+++ service/accountService.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"microservicesAPIDevInGolang/domain"
+	"microservicesAPIDevInGolang/dto"
+	"microservicesAPIDevInGolang/errs"
+	"time"
+)
+
+type AccountService interface {
+	NewAccount(dto.NewAccountRequest) (*dto.NewAccountResponse, *errs.AppError)
+}
+
+type DefaultAccountService struct {
+	repo domain.AccountRepository
+}
+
+func (s DefaultAccountService) NewAccount(req dto.NewAccountRequest) (*dto.NewAccountResponse, *errs.AppError) {
+	a := domain.Account{
+		AccountId:   "",
+		CustomerId:  req.CustomerId,
+		OpeningDate: time.Now().Format("2022-01-01 00:00:00"),
+		AccountType: req.AccountType,
+		Amount:      req.Amount,
+		Status:      "1",
+	}
+	newAccount, err := s.repo.Save(a)
+	if err != nil {
+		return nil, err
+	}
+	response := newAccount.ToNewAccountResponseDto()
+
+	return &response, nil
+}
